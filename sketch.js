82ccda60e312a2dcd50f67e57cd8476d9e397f9d
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-10-24 21:03:23 -0700

    hide and show

--- sketch.js
+++ sketch.js
@@ -1,6 +1,23 @@
+let throwTheSwtch;
+
+let zombieImage;
+
 function setup(){
+noCanvas();
+
+throwTheSwtch = select('#throwTheSwitch')
+zombieImage = select('#zombie')
+zombieImage.hide();
+
+throwTheSwitch.mousePressed(toggleZombie);
 
 }
 function draw(){
-	
+	noBackground();
+}
+
+function toggleZombie(){
+
+zombieImage.show();
+
 }
