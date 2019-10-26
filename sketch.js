b55e044cd9cde361c6d0bc52d60c46b462a1fdc2
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-10-25 17:46:44 -0700

    change

--- sketch.js
+++ sketch.js
@@ -4,25 +4,44 @@ let zombieImage;
 
 let zombieSound;
 
+let x;
+let y;
+
+let xspeed;
+let yspeed;
+
+let ghost;
+
+let background = graveyard;
 
 function preload() {
   soundFormats('wav');
   zombieSound = loadSound('audio/zombie_sound.wav');
 
+ghostie = loadImage('images/ghost.png');
+
 }
 
 function setup() {
-  noCanvas();
+  x = 800;
+  y = 800;
+  xspeed = random(width);
+  yspeed = random(height);
 
   throwTheSwtch = select('#throwTheSwitch')
   zombieImage = select('#zombie')
+  ghost = select('#ghost')
 
 
   throwTheSwtch.mousePressed(toggleZombie);
-
+}
 
 
 function draw() {
+
+                }
+
+
 
 }
 
@@ -35,4 +54,21 @@ function toggleZombie() {
     zombieImage.show();
   }, 500);
 
+}
+
+function ghostie(){
+
+  image(ghost, x, y, 80, 60);
+
+  x = x + xspeed;
+  y = y + yspeed;
+
+  if (x + 80 == width || x == 0){ xspeed =
+   -xspeed;
+     }
+
+  if (y + 60 == height || y == 0){ yspeed =
+   -yspeed;
+
+}
 }
